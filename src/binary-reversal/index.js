0018bec09a8d786611ace8467664b2dd96219b8d
jbchinonso <nwekejohnbosco@gmail.com>
2020-05-07 17:09:26 +0100

    binary-reversal code refactored

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -4,44 +4,21 @@
  *  * @param {string} value
  */
 function binaryReversal(value) {
-    let binaryValue = parseInt(value).toString(2)
+  let binaryValue = parseInt(value).toString(2);
 
-    const binaryArray = binaryValue.split('')
-    let binaryLength = binaryArray.length
-    if(binaryLength < 8 ){
-        while(binaryLength < 8){
-            binaryArray.unshift(0)
-            binaryLength++
-        }
-    }
+  const binaryArray = binaryValue.split("");
 
-    if(binaryLength > 8 && binaryLength < 16){
-        while(binaryLength < 16){
-            binaryArray.unshift(0)
-            binaryLength++
-        }
-    }
+  while (binaryArray.length % 8 !== 0) {
+    binaryArray.unshift(0);
+  }
 
-    if(binaryLength > 16 && binaryLength < 32){
-        while(binaryLength < 32){
-            binaryArray.unshift(0)
-            binaryLength++
-        }
-    }
-    if(binaryLength > 32 && binaryLength < 64){
-        while(binaryLength < 16){
-            binaryArray.unshift(0)
-            binaryLength++
-        }
-    }
-binaryArray.reverse()
+  binaryArray.reverse();
 
-const newString = binaryArray.join('')
+  const newString = binaryArray.join("");
 
-const newValue = parseInt(newString, 2).toString()
+  const newValue = parseInt(newString, 2).toString();
 
-return newValue
-
+  return newValue;
 }
 
 module.exports = binaryReversal;
