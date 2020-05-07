e86deab7811b83f12a0923920ab2f1c800aa8678
Nwike Ugochukwu <bondesvick@gmail.com>
2020-05-07 19:42:03 +0100

    solved list sorting

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,24 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+    if (typeof haystack[0] != "object"){
+        return haystack.lastIndexOf(needle)
+    }
+
+    let row = 0
+    let column = 0
+    let match = false
+
+    haystack.forEach((element, index, array) => {
+      if (element.includes(needle)) {
+        row = index
+        column = element.lastIndexOf(needle)
+        match = true
+      }
+    })
+
+    if (row == 0 && column == 0 && match == false){
+        return -1
+    }
+    return [row, column]
+}
 
 module.exports = listSorting;
