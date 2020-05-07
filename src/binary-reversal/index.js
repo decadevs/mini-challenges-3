ef066277a2982b801b0a46eaa3ed6274ef025e11
Nwike Ugochukwu <bondesvick@gmail.com>
2020-05-07 15:25:30 +0100

    solved binary-reversal challange

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,14 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+    let reversed = Number(value)
+      .toString(2)
+        .padStart(8, "0")
+        .split('')
+        .reverse().join('')
+
+    return parseInt(reversed, 2).toString()
+}
 
 module.exports = binaryReversal;
