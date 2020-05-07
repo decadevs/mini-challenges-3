e3ac584004e148defb985275285f576a99ef68ac
jack <omudavid@gmail.com>
2020-05-07 23:44:44 +0100

    first commit

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,24 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+  if (value < 0) {
+    return 1;
+  }
+  let binary = value.toString(2);
+  binary = binary.split("");
+  var diff = binary.length;
+  if (binary.length < 8) {
+    for (let i = 0; i < 8- diff; i++) {
+      binary.unshift("0");
+    }
+  }
+  binary = binary.reverse(); 
+  binary = binary.join("");
+  let decimal = parseInt(binary,2);
 
+  console.log(decimal);
+    return decimal;
+}
+binaryReversal(111);
 module.exports = binaryReversal;
