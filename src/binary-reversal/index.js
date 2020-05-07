819bfc283fec2f268f340d4bf28842c818739b1d
testbench25 <peperapeng@gmail.com>
2020-05-07 20:40:10 +0100

    added a function to reverse the string

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -4,9 +4,22 @@
  *  * @param {string} value
  */
 function binaryReversal(value) {
+    const binaryStr = value.toString(2);
+    const paddedStr = "00" + binaryStr;
+    const reversedStr = reverseString(paddedStr);
 
     
-}git
+}
+
+function reverseString(str) {
+    let result = "";
+    const start = str.length-1;
+    // loop from the ending to the beginning
+    for(let x = start; x >= 0; x--){
+        result+= str[x];
+    }
+    return result;
+}
 
 
 module.exports = binaryReversal;
