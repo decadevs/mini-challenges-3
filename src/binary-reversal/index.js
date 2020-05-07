147db152aac931f1d169b1ca342227aa9fdc7b10
adeola idowu <adeolaidowu27@gmail.com>
2020-05-07 14:43:32 +0100

    complete binary-reversal challenge

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,13 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+  const binVal = Number(value).toString(2);
+  const binValArr = binVal.split("");
+  while (binValArr.length < 8) {
+    binValArr.unshift("0");
+  }
+  return parseInt(binValArr.reverse().join(""), 2).toString();
+}
 
 module.exports = binaryReversal;
