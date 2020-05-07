58f3529cb7e39c2379bcdf25e57e886f10c6c38f
Lenever <onuorah.ikechukwuvictor@gmail.com>
2020-05-07 13:13:53 +0100

    binary-reversal test passed

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,18 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+  let binaryValue = parseInt(value, 10).toString(2);
+
+  while (binaryValue.length < 8) {
+    binaryValue = "0" + binaryValue;
+  }
+
+  const reverseBinaryValue = binaryValue.split("").reverse().join("");
+
+  const result = parseInt(reverseBinaryValue, 2).toString();
+
+  return result;
+}
 
 module.exports = binaryReversal;
