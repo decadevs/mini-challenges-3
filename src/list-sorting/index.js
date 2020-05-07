76f6aad6e607af7e7a87ec8ba490d3f772d4d09a
adeola idowu <adeolaidowu27@gmail.com>
2020-05-07 15:57:05 +0100

    solve list-sorting challenge

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,25 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+  const arrLength = haystack.length;
+  const answersArray = [];
+  if (typeof haystack[0] == "object") {
+    for (let i = 0; i < arrLength; i++) {
+      haystack[i].lastIndexOf(needle) !== -1 &&
+        answersArray.push([i, haystack[i].lastIndexOf(needle)]);
+    }
+    if (answersArray.length === 0) {
+      return -1;
+    } else if (answersArray.length === 1) {
+      return answersArray[0];
+    } else {
+      return answersArray[answersArray.length - 1];
+    }
+  } else {
+    if (haystack.lastIndexOf(needle) !== -1) {
+      return haystack.lastIndexOf(needle);
+    } else {
+      return -1;
+    }
+  }
+}
 
 module.exports = listSorting;
