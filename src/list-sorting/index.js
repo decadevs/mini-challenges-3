3e9d3fdd94903a7cf4fa24982e86355e3ca23a67
simplyking-decadev <56389903+simplyking-decadev@users.noreply.github.com>
2020-05-07 17:04:11 +0100

    highlight steps to returning last find index of needle in haystack

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,17 @@
-function listSorting(needle, haystack) {}
+/*
+    steps to list sorting
+
+    1. check if haystack is empty return -1
+    2. for multi-dimensional array, perform a match check on every haystack against the needle-
+        a. keep position dynamic to take the new row,col match every time
+        b. if no row,col match return -1
+    3. for one dimensional array, perform a match check on every haystack against the needle-
+        a. keep position dynamic for the match location
+        b. if no position found, return -1
+*/
+
+function listSorting(needle, haystack) {
+
+}
 
 module.exports = listSorting;
