12207734ad7d1014266cee19696a93e2f91fafa9
olumaja <olumajasegun@gmail.com>
2020-05-07 10:56:30 +0100

    list-sorting challenge solved

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,30 @@
-function listSorting(needle, haystack) {}
+"use strict";
+
+function listSorting(needle, haystack) {
+  let result;
+  let needleExist = false;
+
+
+  if(haystack.some(x => Array.isArray(x))){
+
+    for(let i = 0; i < haystack.length; i++){
+
+      if(haystack[i].length === 0){return -1}
+
+      if(haystack[i].includes(needle)){
+        result = [i, haystack[i].lastIndexOf(needle)];
+        needleExist = true;
+      }
+      else if(!needleExist){result = -1}
+
+    }
+
+    return result;
+  }
+
+  return haystack.lastIndexOf(needle);
+
+
+}
 
 module.exports = listSorting;
