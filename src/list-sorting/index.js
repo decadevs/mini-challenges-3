00a40488737e30c02ee2af5ea3c48054c8e7e77c
Kaosilisochukwu Nwizu <50839130+Kaosilisochukwu@users.noreply.github.com>
2020-05-07 17:01:57 +0100

    implement listSorting function

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,12 @@
-function listSorting(needle, haystack) {}
-
+function listSorting(needle, haystack) {
+  if (typeof (haystack[0]) !== "object") {
+    return haystack.lastIndexOf(needle);
+  }
+  for(let i = haystack.length - 1; i >= 0; i -= 1)  {
+    if (haystack[i].includes(needle)) {
+      return [i, haystack[i].lastIndexOf(needle)]
+    }
+  };
+  return -1
+}
 module.exports = listSorting;
