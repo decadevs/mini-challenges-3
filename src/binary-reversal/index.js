9854e2ce2130bf8546687244fa783036c37175db
Oche Ejeh <35895581+goodnessemmanuel@users.noreply.github.com>
2020-05-08 00:15:56 +0100

    Update index.js

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -55,7 +55,7 @@ function binaryReversal(value)
                 power--;
             }
 
-            return valueReversed;        
+            return `${valueReversed}`;        
         }
     }
 }
