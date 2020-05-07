538b397347b283f512eca8010d618b1804c5c101
jbchinonso <nwekejohnbosco@gmail.com>
2020-05-07 19:12:06 +0100

    all test for binary-sorting passed

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,29 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+    // check if the haystack is a multidimensional array
+    let multidimension = false;
+    for (let element of haystack) {
+      if (typeof element === "object") {
+        multidimension = true;
+      }
+    }
+    
+    // if what we are dealing with is a multidimensional array then we loop 
+    if (multidimension) {
+      const arrayCollector = [];
+      for (let row in haystack) {
+        if (haystack[row].includes(needle)) {
+          arrayCollector.push([+row, haystack[row].lastIndexOf(needle)]);
+        }
+      }
+      const result = arrayCollector.length !== 0 ?  arrayCollector[arrayCollector.length - 1] :  -1
+      return result
+    }
+    
+    //if haystack is not a multidimensional array then do this simple operation
+    if (typeof haystack.lastIndexOf(needle) !== "undefined") {
+      return haystack.lastIndexOf(needle);
+    }
+    return -1;
+  }
 
 module.exports = listSorting;
