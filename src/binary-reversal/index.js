74b969af2777849a2f14e238d852b78007ec893d
Dexkode <dexkode4@gmail.com>
2020-05-07 23:31:13 +0100

    first commit

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,14 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
-
+function binaryReversal(value) {
+  const PAD = 8;
+  const reversedBinary = parseInt(value)
+    .toString(2)
+    .padStart(PAD, "0")
+    .split("")
+    .reverse()
+    .join("");
+  return parseInt(reversedBinary, 2).toString();
+}
 module.exports = binaryReversal;
