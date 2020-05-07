76eb4bf431d64d1cd6b0a19095340fbdb25142c8
teeliny <teeliny@gmail.com>
2020-05-07 18:48:31 +0100

    mini-challenges-3 index files updated

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,36 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+  let binary = parseInt(value).toString(2);
+  let binaryString = binary.toString();
+  let binaryArray = binaryString.split('');
+  
+  let remainder = binary.length % 8;
+  let zerosToBeAdded
+  if (remainder !== 0) {
+    zerosToBeAdded = 8 - remainder;
+  }
+  else {
+    zerosToBeAdded = 0;
+  }
+
+  let zeroArray = [];
+  for (let i = 0; i < zerosToBeAdded; i++) {
+    zeroArray.push('0');
+  }
+
+  binaryPadding = [...zeroArray,...binaryArray];
+  let reversePadding = binaryPadding.reverse();
+
+  let reverseBinary = '';
+  for (let j = 0; j < reversePadding.length; j++) {
+    reverseBinary += reversePadding[j];
+  }
+
+  let  decBinDec = parseInt(reverseBinary,2);
+  let ans = decBinDec.toString();
+  return ans;
+}
 
 module.exports = binaryReversal;
--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,26 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+  if (haystack.length === 0) {
+    return -1;
+ }
+ else if (haystack[0].constructor !== Array) {
+    let possibleIndex = haystack.lastIndexOf(needle);
+    return possibleIndex;
+ }
+ else {
+    let possibleIndex =  [];
+    for (let i = 0; i < haystack.length; i++) {
+      if (haystack[i].indexOf(needle) !== -1) {
+        possibleIndex.push([i,haystack[i].lastIndexOf(needle)]);
+      }
+    }
+    if (possibleIndex.length === 0) {
+      return -1;
+    }
+    else {
+      let realIndex = possibleIndex[possibleIndex.length-1];
+      return realIndex;
+    }
+ }
+}
 
 module.exports = listSorting;
