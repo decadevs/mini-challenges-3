3f99908c9c0253e52d57e41b45acfe80fe2bd378
Thommzy <tomiwaobeisun@yahoo.com>
2020-05-07 21:47:38 +0100

    list-sorting

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,15 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+  let haystackLength = haystack.length;
+
+  for (let j = haystackLength - 1; j >= 0; j--) {
+    let index = haystack[j];
+
+    if (typeof index != "object") return haystack.lastIndexOf(needle);
+
+    if (index.lastIndexOf(needle) > -1) return [j, index.lastIndexOf(needle)];
+  }
+
+  return -1;
+}
 
 module.exports = listSorting;
