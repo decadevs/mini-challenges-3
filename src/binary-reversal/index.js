dc84a882f86798876cf0e3040bd174506890a9fb
Iretii0511 <simonjulius0511@gmail.com>
2020-05-07 20:24:59 +0100

    algorithm all passed in the first try

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,19 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+  const VALUE_TO_BINARY = parseInt(value).toString(2);
+  let reversedBinaryArray = VALUE_TO_BINARY.split("").reverse();
+  if (reversedBinaryArray.length % 8 === 0) {
+    const REVERSED_BINARY_STRING = reversedBinaryArray.join("");
+    return parseInt(REVERSED_BINARY_STRING, 2).toString();
+  } else {
+    for (let i = 0; i < reversedBinaryArray.length % 8; ++i) {
+      reversedBinaryArray.push("0");
+    }
+    const REVERSED_BINARY_STRING = reversedBinaryArray.join("");
+    return parseInt(REVERSED_BINARY_STRING, 2).toString();
+  }
+}
 
 module.exports = binaryReversal;
