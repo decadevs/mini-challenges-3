b82a0b9f2ab9c1e702a69d38cd334b1619677ca1
testbench25 <peperapeng@gmail.com>
2020-05-08 23:53:24 +0100

    solved the List-Sorting Algo

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,5 +1,21 @@
 function listSorting(needle, haystack) {
-    
+    const arrayLength =  haystack.length;
+    // if the array is not a nested array
+    for (let x= arrayLength-1; x>=0; x++){
+        if(haystack[x] == needle) return x;
+
+        // if the current element is an array
+        nestedArr = typeof(haystack[x]) == 'object';
+        if(nestedArr) {
+            let start = haystack[x].length-1;
+            for (y = start; y>=0; y--){
+                if(haystack[x][y] == needle) return [x,y];
+            }
+        }
+
+    }
+    return -1
 }
+
 
 module.exports = listSorting;
