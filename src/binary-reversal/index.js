4ab28b44c2a44f944afbdabc7dfc6e6976320476
Mandong Lawrence <Mandonglawrence@gmail.com>
2020-05-08 13:40:32 +0100

    run npm install

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,38 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+
+	let newArr = [];
+	let n = parseInt(value);
+	let decimalNumber=0;
+	let multipliers=[];
+
+	while(n>0){
+		newArr.push(n%2);
+		n=Math.floor(n/2);
+	}
+
+
+	if(newArr.length<8){
+		let b = 8-newArr.length;
+		while(b>0){
+			newArr.push(0);
+			b--;
+		}
+	}
+
+  x=newArr.length-1;
+  while(x>=0){
+  	multipliers.push(x);
+  	x--;
+  }
+
+  for (let i = 0; i < newArr.length; i++) {
+  	decimalNumber+=(newArr[i]*Math.pow(2,multipliers[i]));
+  }
+
+  return decimalNumber+"";
+}
 
 module.exports = binaryReversal;
--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,23 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+	let result=[];
+	
+	if (typeof(haystack[0])==="object") {
+         haystack.forEach((a,i)=>{
+      	if(a.lastIndexOf(needle)!=-1){
+      		result.push([i,a.lastIndexOf(needle)]);
+      	}
+      })
+
+      if(result.length>0){
+      	return ( result[result.length-1])
+      }else{
+      return -1
+  		}
+  }
+		
+	return haystack.lastIndexOf(needle)
+		
+	
+}
 
 module.exports = listSorting;
