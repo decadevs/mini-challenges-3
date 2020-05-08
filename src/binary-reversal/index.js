5aff3e6229b1489e6a19a32bc5108d8ee622fc7a
Lenever <onuorah.ikechukwuvictor@gmail.com>
2020-05-08 18:02:22 +0100

    All test passed and code commented

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -4,14 +4,18 @@
  *  * @param {string} value
  */
 function binaryReversal(value) {
+  //Converting value to a binary string
   let binaryValue = parseInt(value, 10).toString(2);
 
+  // Adding zero if to make up length to 8
   while (binaryValue.length < 8) {
     binaryValue = "0" + binaryValue;
   }
 
+  // reversing binary
   const reverseBinaryValue = binaryValue.split("").reverse().join("");
 
+  // converting to decimal value
   const result = parseInt(reverseBinaryValue, 2).toString();
 
   return result;
