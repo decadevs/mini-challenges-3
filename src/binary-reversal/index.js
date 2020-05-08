3d992b6364ee5a3e1f7090c53913e3d030837df9
jack <omudavid@gmail.com>
2020-05-08 18:31:18 +0100

    final scommit

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -7,20 +7,16 @@ function binaryReversal(value) {
   if (value < 0) {
     return 1;
   }
-  let binary = value.toString(2);
+  let binary = parseInt(value).toString(2);
   binary = binary.split("");
-  
-  if ((binary.length) % 8 !=0) 
-  {
-    var diff = binary.length % 8;
-    for (let i = 0; i < 8 - diff; i++) {
+  var diff = 8 - binary.length ;
+  for (let i = 0; i < diff; i++) {
       binary.unshift("0");
-    }
   }
   binary = binary.reverse(); 
   binary = binary.join("");
-  let decimal = parseInt(binary,2);
-  decimal= decimal.toString();
+  var decimal = parseInt(binary, 2);
+  decimal = decimal.toString();
   return decimal;
 }
 module.exports = binaryReversal;
