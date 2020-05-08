d9d4076b066b668fea011c09a77b2c0f64bbc726
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-05-08 19:39:03 +0100

    add code for list-sorting

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,22 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+ let ret = -1;
+
+  if (typeof haystack[0] == "array" || typeof haystack[0] == "object") {
+    // its multi-dimentional
+    // let row = -1,
+      col = -1;
+    for (let index = 0; index < haystack.length; index++) {
+      if (haystack[index].lastIndexOf(needle) >= 0) {
+        row = index;
+        col = haystack[index].lastIndexOf(needle);
+      }
+    }
+    if (col >= 0 && row >= 0) ret = [row, col];
+  } else {
+    // its single dimensional
+    ret = haystack.lastIndexOf(needle);
+  }
+  return ret;
+}
 
 module.exports = listSorting;
