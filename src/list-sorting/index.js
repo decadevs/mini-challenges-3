7709784f997412de6e5e720044fe8d5e299d694c
David Oparanti <david.oparanti@gmail.com>
2020-05-08 17:09:26 +0100

    Render output correctly

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,5 +1,30 @@
 function listSorting(needle, haystack) {
-    let arr = [];
+    //debugger
+    let needleIndex;
+    let arrayIndex;
+
+    for (let val of haystack) {
+        if (Array.isArray(val)) {
+            for (let i = 0; i < val.length; i++) {
+                if (val[i] === needle) {
+                    arrayIndex = haystack.indexOf(val);
+                    needleIndex = val.indexOf(needle);
+                }
+            }
+
+
+        } else {
+            needleIndex = haystack.indexOf(needle);
+        }
+
+
+    }
+      
+    if (arrayIndex) {
+        return `[${arrayIndex}, ${needleIndex}]`;
+    }
+    return needleIndex + "";
+
 }
 
 module.exports = listSorting;
