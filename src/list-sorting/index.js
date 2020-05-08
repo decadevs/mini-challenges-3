f2292827f7a04199d9e71332232ed8a39add257e
Dexkode <dexkode4@gmail.com>
2020-05-08 13:42:16 +0100

    list-sorting algo

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,15 @@
-function listSorting(needle, haystack) {}
-
+function listSorting(needle, haystack) {
+  let json = haystack;
+  let row = haystack.length - 1;
+  // check if  array is flat
+  if (JSON.stringify(json)[1] !== "[") {
+    return haystack.lastIndexOf(needle);
+  }
+  //multidimension array
+  for (let i = row; i >= 0; i--) {
+    if (haystack[i].lastIndexOf(needle) !== -1)
+      return new Array(i, haystack[i].lastIndexOf(needle));
+  }
+  return -1;
+}
 module.exports = listSorting;
