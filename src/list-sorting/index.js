ff18b5777e7278d0ce373bb568f43f2477751500
morlimoore <cooliyk2@gmail.com>
2020-05-08 12:54:07 +0100

    Solved list-sorting

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,21 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+  if (!haystack || !Array.isArray(haystack)) {
+    throw Error('Please provide a valid haystack')
+  }
+
+  if (!Array.isArray(haystack[0])) {
+    return haystack.lastIndexOf(needle);
+  }
+
+  let answer = [];
+  let index = haystack.length - 1;
+  for (index; index > -1; index--) {
+    if (haystack[index].includes(needle)) {
+      answer.push(index, haystack[index].lastIndexOf(needle));
+      return answer;
+    }
+  }
+  return -1;
+}
 
 module.exports = listSorting;
