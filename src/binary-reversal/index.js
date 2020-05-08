54afcf19eef9ec8a991f37c5712feb785940a0b4
euchigere <ugochigere02@gmail.com>
2020-05-08 08:10:35 +0000

    complete binary reversal solution

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,29 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+    if (typeof value !== "string") {
+        throw Error("Invalid value, expected string value");
+    }
+
+    let number = Number(value);
+
+    if (!number || number < 0) {
+        throw Error("Please enter a valid string");
+    }
+
+    let binaryArray = number.toString(2).split("")
+    const LEN = binaryArray.length;
+    const BYTE_LENGTH = 8;
+
+    let binaryPadded;
+    if (LEN < BYTE_LENGTH) {
+        binaryArray.unshift("0".repeat(BYTE_LENGTH - LEN));
+    }
+
+    const REVERSE_STRING = binaryArray.reverse().join("")
+    return parseInt(REVERSE_STRING, 2).toString(10);
+
+}
 
 module.exports = binaryReversal;
