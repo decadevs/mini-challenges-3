d82b632e6e773657a95720b27f94c2dc7bce06e5
mykelgozie <mykelgozie@gmail.com>
2020-05-08 13:17:12 -0700

    just completed all challenge 3 task

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,97 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+
+    let lastIndex;
+
+
+
+    if (haystack[0] == undefined) {
+
+        lastIndex = -1;
+        
+    }else {
+  
+
+    
+
+
+    if (haystack[0][0]== undefined){
+
+        for (let index = haystack.length -1; index >= 0; index--) {
+
+                if (haystack[index] == needle) {
+
+
+                    lastIndex = index;
+                    
+                break;
+            
+                    
+                } else {
+
+                    lastIndex = -1;
+                }
+  
+         
+              
+                
+        }
+          
+
+
+    } else {
+
+        let count = 0;
+       
+        for (let index = haystack.length -1; index >= 0; index--) {
+                    if (count == 1){
+                        break;
+                    }
+
+          for (let b = haystack[index].length -1; b >= 0 ; b--) {
+
+                    if (count == 1){
+                        break;
+                    }
+            
+                    if (haystack[index][b] == needle) {
+
+                            count = count + 1;
+
+
+                          lastIndex = [];
+                          lastIndex[0]  = index;
+                          lastIndex[1] = b;
+                     
+
+
+                        
+                    } else {
+
+
+                        lastIndex = -1;
+                    }
+
+                   
+              
+          }
+
+          
+
+        }
+
+
+    }
+
+}
+
+
+    return lastIndex;
+
+
+
+
+
+
+}
 
 module.exports = listSorting;
