8047e839b2110f8a0b988f49684870d913232a67
Akius1 <andrewurom@gmail.com>
2020-05-09 04:01:31 -0700

    Done with list-sorting

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,32 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+
+   let stack = [];
+    // loop through haystack to check if elem of haystack is an array of integer or an integer
+   for(let elem of haystack){
+       if(Array.isArray(elem)){
+           if(elem.includes(needle)){
+               let rowIdx = haystack.indexOf(elem);
+               let colIdx = elem.lastIndexOf(needle);
+               let arrIndex = [rowIdx, colIdx];
+               
+               stack.push(arrIndex);
+           }
+           if(elem.length === 0){
+               return -1
+           }
+       }else{
+           if(haystack.includes(needle)){
+                let idx = haystack.lastIndexOf(needle);
+                stack.push(idx);
+           }else{
+               stack.push(-1)
+           }
+                
+       }
+   }
+
+   let result = stack.pop();
+   return (haystack.length!==0 && result !== undefined) ? result: -1;
+}
 
 module.exports = listSorting;
