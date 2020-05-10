ac23b1c9a1b0475e2fa33e06531ea9d475a44306
Ayodeji97 <danielayodeji97@gmail.com>
2020-05-10 07:40:08 +0100

    mini-challenge_3 added

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,31 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+  // check if the haystack is an one dimensional array or multi
+  
+  var myFirstIndexPosition = [];
+  var myLastIndexPosition = [];
+  var myReturnPosition = [];
+  for(var i = 0; i < haystack.length; i++){
+      if(haystack[i].includes(needle)){
+          myFirstIndexPosition.push(true);
+          myLastIndexPosition.push(haystack[i].lastIndexOf(needle));
+      } else{
+          myFirstIndexPosition.push(false);
+          myLastIndexPosition.push(false);
+      }
+  }
+
+
+  if(myFirstIndexPosition.includes(true)){
+      myReturnPosition.push(myFirstIndexPosition.lastIndexOf(true), (myLastIndexPosition[myFirstIndexPosition.lastIndexOf(true)]));
+      // console.log(myReturnPosition);
+      return myReturnPosition;
+  } else{
+      return -1;
+  }
+
+
+ }
+
+
 
 module.exports = listSorting;
