18a62d06f8bb7b177e1fb15d3e919f093d5dbe4f
Akius1 <andrewurom@gmail.com>
2020-05-10 15:02:38 -0700

    Done with binary reversal

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,20 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+    //convert to binary
+    let positiveInt = +value;    
+    let binary = positiveInt.toString(2);
+    // Check if binary is eight bit if not add 00
+    while(binary.length < 8){
+        binary = 0 + binary;
+    }
+    // reverse binary
+    let reverseBinary = binary.split('').reverse().join('');
+    // covert back to number
+    let digit = parseInt(reverseBinary, 2);
+    // Return result
+    return digit.toString();
+}
 
 module.exports = binaryReversal;
