8c44033c94feff85f18be47eb3f4e7f25014e695
testbench25 <peperapeng@gmail.com>
2020-05-10 16:13:50 +0100

    rearranged the program logic

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,10 +1,7 @@
 function listSorting(needle, haystack) {
     const arrayLength =  haystack.length;
     for (let x= arrayLength-1; x>=0; x--){
-        // if the current element is not an array
-        if(haystack[x] == needle) return x;
-
-        // if the current element is an array
+      // if the current element is an array
         nestedArr = typeof(haystack[x]) == 'object';
         if(nestedArr) {
             let start = haystack[x].length-1;
@@ -12,6 +9,8 @@ function listSorting(needle, haystack) {
                 if(haystack[x][y] == needle) return [x,y];
             }
         }
+        // if the current element is not an array
+        if(haystack[x] == needle) return x;
 
     }
     return -1
