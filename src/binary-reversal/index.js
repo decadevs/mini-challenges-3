ce6f45db4d7a16d3f70ca1b09a16489585673654
faith okpala <okpalafaith1@gmail.com>
2020-06-08 11:11:33 +0100

    solution

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,22 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+
+    let numberValue = parseInt(value).toString(2);
+    let numberValueReversed = "";
+
+    while (numberValue.length < 8) {
+        numberValue = "0" + numberValue;
+    }
+
+    for (let i = numberValue.length - 1; i >= 0; i--) {
+        numberValueReversed += numberValue[i]
+    }
+
+    return parseInt(numberValueReversed, 2).toString();
+
+
+}
 
 module.exports = binaryReversal;
--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,23 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+
+    let index = [];
+    for (let i = haystack.length - 1; i >= 0; i--) {
+        if (Array.isArray(haystack[i])) {
+            for (let j = haystack[i].length; j >= 0; j--) {
+                if (haystack[i][j] == needle) {
+                    index.push(i, j);
+                    return index;
+                }
+            }
+        } else {
+            for (let i = haystack.length - 1; i >= 0; i--) {
+                if (haystack[i] === needle) return i;
+            }
+
+        }
+    }
+
+    return -1;
+}
 
 module.exports = listSorting;
