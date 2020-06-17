d9dc365f2818940df11cfb28bee21dfea90a94c1
Adewale Nurudeen <nurudeenadeolaa@gmail.com>
2020-06-16 17:13:59 -0700

    worked on case for one dimensional array in list sorting

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,8 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+  let result = haystack.lastIndexOf(needle)
+  return result
+}
+
+console.log(listSorting(4, [1,1,2,2,3,3,4,5,6,4,2,3,4,3]))
 
 module.exports = listSorting;
