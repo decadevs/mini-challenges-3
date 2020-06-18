da313424d39ff68a27d6cb33ade8abbf3aff594e
daphycisist <daphycisist@gmail.com>
2020-06-18 23:57:25 +0100

    add list-sorting algorithm solution

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,20 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+  let convertedDecimal
+  let reversedBin
+  let binaryVal = parseInt(value).toString(2); 
+
+  while(binaryVal.length<8) {
+    binaryVal = "0" + binaryVal
+  }
+  
+  reversedBin = binaryVal.split('').reverse().join(''); 
+  
+  convertedDecimal = parseInt(reversedBin,2).toString(); 
+  
+  return convertedDecimal;
+}
 
 module.exports = binaryReversal;
