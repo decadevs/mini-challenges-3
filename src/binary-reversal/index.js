0e718684b0b26003a89785cad6caaccf50765ca6
warami13 <wrmyeresanara@gmail.com>
2020-06-19 04:28:25 +0100

    add solution to binary-reversal

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,33 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+    // get the number value of the value in the funtion
+    let numVal = parseInt(value);
+    // change the number value to binary form
+    let binVal = numVal.toString(2);
+    // then change it to an array to loop
+    let binArr = binVal.split("");
+    // console.log(binArr);
+
+    //while the length of the arr / 8 is not equal to 0 keep adding 0 to the beginning of the arr to pad it to the nearest 8 bits
+    while (binArr.length % 8 !== 0) {
+        binArr.unshift(0)
+    }
+    // console.log(binArr);
+    binArr.reverse();
+
+    // create a new string to hold the new reversed string
+    let newRevStr = binArr.join("");
+    // console.log(newRevStr);
+
+    //return the decimal form of the reversed str
+    let decimalVal = parseInt(newRevStr, 2);
+    console.log(decimalVal);
+
+    return decimalVal.toString();
+
+}
+console.log(binaryReversal("10")); //128
 
 module.exports = binaryReversal;
