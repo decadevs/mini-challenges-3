b878091a82628976473e42474d8ee40351db1f72
Oyinkansola Alabi <jessyinks14@gmail.com>
2020-07-05 21:10:57 +0100

    Add Oyinkansola's week 2 task

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,15 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
-
+function binaryReversal(value) {
+    //     convert string to decimal
+        let parsedValue = parseInt(value, 10);
+    // convert decimal to string 
+     let stringedValue = parsedValue.toString(2)
+    //     split and reverse string
+    let splitedValue =(stringedValue.padStart(8,'0').split("").reverse().join(""));
+    console.log(splitedValue);
+    let reversedDecimal = parseInt(splitedValue, 2);
+    return reversedDecimal.toString(10);
+    }
 module.exports = binaryReversal;
--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,29 @@
-function listSorting(needle, haystack) {}
-
+function listSorting(needle, haystack) {
+  let store = [];
+  for (let row = 0; row < haystack.length; row++) {
+    //         if haystack is a one-dimensional array
+    if (typeof haystack[row] === "number") {
+      return haystack.lastIndexOf(needle);
+    } else {
+      //     if haystack is multi-dimensional
+      for (let row = 0; row < haystack.length; row++) {
+        for (let column = 0; column < haystack[row].length; column++) {
+          if (haystack[row][column] === needle) {
+            if (row > 0) {
+              store.length = 0;
+              store.push(row);
+              store.push(column);
+            } else {
+              store.push(column);
+            }
+          }
+        }
+      }
+    }
+  }
+  if (store.length == 0) {
+    return -1;
+  }
+  return store;
+}
 module.exports = listSorting;
