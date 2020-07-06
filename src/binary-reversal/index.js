7e0b5602ee462136f670868ff56755ad6f90df48
ndipbanyan <agnesjedidiah09@gmail.com>
2020-07-06 09:14:01 +0100

    committing all at once:bad practice

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,18 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) 
+{
+
+    let decimalNumber=parseInt(value);//convert value to Integer
+
+        decimalNumber=decimalNumber.toString(2).padStart(8, 0); //convert  valueto string binary
+
+        decimalNumber=decimalNumber.split("").reverse().join(""); //Split and reverse then join the reversed binary value
+
+        decimalNumber= parseInt(decimalNumber,2); //convert binary value to decimal number
+        decimalNumber=decimalNumber.toString(); // convert decimal number to a string value
+        return decimalNumber;
+}
 
 module.exports = binaryReversal;
--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,43 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) 
+{   
+    
+
+    for (let index = haystack.length-1; index>=0; index--)
+    {   
+        //compute for a single array
+        if(typeof haystack[index] === 'number')
+        {
+            return haystack.lastIndexOf(needle)
+        }
+
+        //compute for multidimensional array
+        if(haystack[index].lastIndexOf(needle) > -1 )
+        {
+            return [index , haystack[index].lastIndexOf(needle)]
+        }
+
+        
+    }
+    return -1;
+    /*let positionX;
+    let positionY;
+    let positionXY;
+    for (let i=0; i<haystack.length; i++) 
+    {
+      for (let j=0; j<haystack[i].length; j++) 
+      {
+        if (haystack[i][j] == needle) 
+        { 
+          positionX =i; 
+          positionY=j;
+          positionXY=[positionX,positionY];
+          return positionXY;  
+        } 
+            
+        }
+        return -1;
+      }*/
+      
+}
 
 module.exports = listSorting;
