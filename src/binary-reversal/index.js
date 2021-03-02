399bd58872133d4a95b43fa30a608a1a708e44b4
ObioraM <obioramaduakor@gmail.com>
2021-03-02 14:45:42 +0100

    Complete mini-challenges-3

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,20 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+    let num = parseInt(value);
+    let binaryNum = num.toString(2);
+
+    for (let i = 8-binaryNum.length; i > 0; i--) {
+        binaryNum = "0"+binaryNum;
+    }
+
+    let reversedBinaryNum = "";
+
+    for (let i= binaryNum.length -1; i>=0; i--){
+        reversedBinaryNum += binaryNum[i];
+    }
+    return parseInt(reversedBinaryNum, 2).toString();
+}
 
 module.exports = binaryReversal;
--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,28 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+    let result = -1;
+
+    if (haystack.length == 0){
+        return result;
+    }
+
+    if (haystack[0].length != undefined){
+        for (let i=haystack.length - 1; i>=0; i--) {
+            for (let j=haystack[i].length - 1; j>=0; j--) {
+                if (haystack[i][j]==needle) {
+                    result = [i, j];
+                    return result;
+                }
+            }
+        }
+    } else {
+        for (let i=haystack.length - 1; i>=0; i--) {
+            if (haystack[i]==needle) {
+                result = i;
+                return result;
+            }
+        }
+    }
+    return result;
+}
 
 module.exports = listSorting;
