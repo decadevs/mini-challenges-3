dcd9dc4133f920342386be7f2a10ae1758852957
Ebuka <bukasin1@gmail.com>
2021-06-28 11:58:34 +0100

    commits

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,31 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+    let pos
+    for(i in haystack){
+        if(Array.isArray(haystack[i])){
+            if(haystack[i].includes(needle)){
+                for(j in haystack[i]){
+                    if(haystack[i][j] === needle){
+                        pos = [Number(i) , Number(j)]
+                    }
+                }
+            }
+        }else{
+            if(haystack[i] === needle){
+                pos = Number(i)
+            }
+        }
+    }
 
+    return pos === undefined ? -1 : pos
+
+}
+
+console.log(listSorting(1, [
+    [1],
+    [2, 1],
+    [3, 1, 2],
+    [1, 2, 3, 4],
+    [5, 3, 4, 2, 1],
+    [0, 0, 0, 0, 0],
+  ]))
 module.exports = listSorting;
