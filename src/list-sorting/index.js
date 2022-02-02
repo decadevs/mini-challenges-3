672d4b85fe8d0a529a86b3c4a01d2cef54ed8bc7
Decagon <decagon@Decagons-MacBook-Air.local>
2022-02-02 09:31:35 +0100

    mini 3 final commit

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,35 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+  //initialise result to -1 as the case when the needle is not found
+  let result = -1
+
+  //Loop through the array of haystack to search needle in descending order
+  for (let i = haystack.length - 1; i >= 0; i--) {
+    //Check if needle is there and the corresponding index
+      if (Array.isArray(haystack[i])) {
+          let index = haystack[i].lastIndexOf(needle)
+          //Return the -1 if not found and assign the index to result if found
+          if (index != -1) {
+              result = [i, index]
+              return result
+          }
+
+      } else {
+          if (haystack[i] === needle) {
+              result = i
+              return result
+          }
+      }
+  }
+  return result
+
+}
+
+// console.log(listSorting(8, [
+//   [1, 2, 3, 4, 8],
+//   [1, 2, 3, 4, 5],
+//   5,
+// ]))
 
 module.exports = listSorting;
+
+
