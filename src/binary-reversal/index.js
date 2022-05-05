ec2ad8ee7fc17206495fa846080516fdad8cc734
Joshua <jadeyemo002@gmail.com>
2022-05-05 12:50:11 +0100

    solved

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,26 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+    let binary = (value >>> 0).toString(2).split('')
+    let addPad = 0
+    let checkLength = 8 - binary.length
+
+    if (checkLength > 0) {
+        for (let i = 0; i < checkLength; i++) {
+            binary.unshift(addPad)
+        }
+    }
+    
+    if (checkLength < 0) {
+        for (let i = checkLength; i >= 0; i++) {
+            redo = binary.shift()
+        }
+    }
+
+    let reverseForm = binary.reverse().join('')
+    let decimalConversion = parseInt(reverseForm, 2)
+    return decimalConversion
+}
 
 module.exports = binaryReversal;
