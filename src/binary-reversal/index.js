bf8958c314c204cdd0a3547e7eebd3cb2cdd600e
Bonaventure Umeokwonna <bonaventure.umeokwonna@decagon.dev>
2022-05-06 11:12:59 +0100

    bonaventure solution challenge-3

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,30 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+    let binary = "";
+    while(value != 0){
+        let b = value % 2;
+        binary += b;
+        value = Math.trunc(value / 2);
+    }
 
+    let zero_fills = ((8 - binary.length) > 0 ? "0".repeat((8 - binary.length)) : "" );
+    binary += zero_fills;
+    
+    // since this is already in reverse pattern no need for reversal
+    // convert to decimal
+    let decimal = 0;
+    let p = binary.length - 1;
+    for(let i = 0; i < binary.length; i++){
+        val = Number(binary[i]) * Math.pow(2, p);
+        decimal += val;
+        p--;
+    }
+    return decimal;
+
+    
+    
+}
+console.log(binaryReversal(47));
 module.exports = binaryReversal;
