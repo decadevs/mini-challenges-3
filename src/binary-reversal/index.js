c40980267d0b095aa8d69ed0c72fcb347d05bd58
tofarati <oluwatofarati.agbaje@decagon.dev>
2022-06-18 04:22:46 +0100

    "Solution to Mini Challenges 3"

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,22 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value)
+{
+    let temp = parseInt(value);
+    temp = temp.toString(2);    //binary representation
+    temp = temp.split('');
+
+    //pad bits if not complete bytes
+    if (temp.length % 8 !== 0)
+    {
+        let missingBits = 8 - (temp.length % 8);
+        let leadingZeros = new Array(missingBits).fill('0');
+        temp = leadingZeros.concat(temp);
+    }
+
+    temp = temp.reverse().join('');
+    return String(parseInt(temp, 2)); //get the result in decimal and cast to string
+}
 
 module.exports = binaryReversal;
--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,29 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack)
+{
+  let lastIndex = -1;
+  //traverse array rows backwards to spot last appearance first
+  for (let i = haystack.length; i > 0; i--)
+  {
+    //value at index is iterable if iterator is a function
+    if (typeof haystack[i - 1][Symbol.iterator] === 'function')
+    {
+      //multidimensional logic- traverse columns backwards also
+      for (let j = haystack[i - 1].length; j > 0; j--)
+      {
+        //check for needle and return array of row and column indexes if found
+        if (haystack[i - 1][j - 1] === needle)
+        {
+          return [i - 1, j - 1];
+        }
+      }
+    }
+    //1d array- check for needle and return index if found
+    else if (haystack[i - 1] === needle)
+    {
+      return i - 1;
+    }
+  }
+  return lastIndex;
+}
 
 module.exports = listSorting;
