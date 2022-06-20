8ec21f57a3b61d101e0cd01bb2a3b45863b33d08
Dec <dec@Samson-Ajulor.local>
2022-06-20 20:40:58 +0100

    completed mini-challange-3

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,20 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+      
+      let binary = Number(value).toString(2)
+      let eightBit = `0000000${binary}`.slice(-8)
+      let reverse ="";
+      for(let i = eightBit.length-1;i>=0;i--){
+         reverse +=eightBit[i];
+      }
+     let decimal = parseInt(reverse,2);
+     
+     return String(decimal);
+   
+   
+}
 
+//console.log(binaryReversal("45"))
 module.exports = binaryReversal;
