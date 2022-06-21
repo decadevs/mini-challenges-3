0211c255855e50cdef4b3cd91488ae43affb4f93
Chukwuma Michael Ezeobika <mezeobika32@gmail.com>
2022-06-21 19:53:53 -0400

    Assignment Three

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,23 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+    const valueNum = Number(value);
+    console.log(valueNum);
+
+    const binary = valueNum.toString(2)
+    console.log(typeof binary);
+    let reverseBinary = binary.split('').reverse().join('');
+
+    while (reverseBinary.length < 8) {
+        reverseBinary += 0;
+    }
+    console.log(reverseBinary);
+
+    const outputValue = parseInt(reverseBinary, 2);
+    console.log(typeof outputValue);
+    return outputValue.toString();
+}
+console.log(binaryReversal('47'));
 
 module.exports = binaryReversal;
--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,22 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+    // if (Array.isArray(haystack[0])) {
+    //     for (let i = 0; i < haystack.length; i++) {
+    //         if (arr[i].includes(needle)) {
+    //             return 
+    //         }
+    //     }
+    // } else {
+    //     return haystack.lastIndexOf(needle);
+    // }
+
+    if(!Array.isArray(haystack[0])) return haystack.lastIndexOf(needle);
+    for (let i = haystack.length - 1; i >=0; i--) {
+        let value = haystack[i].lastIndexOf(needle);
+        if ( value !== -1) return [i, value];
+    }
+    return -1;
+}
+
+console.log(listSorting(5, [1, 2, 3, 4, 8, 5]));
 
 module.exports = listSorting;
