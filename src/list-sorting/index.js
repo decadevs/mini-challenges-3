2d127cf4e58e6c1903a77c6c3a29a38f5769efe9
Dec <dec@Oluwaseun.local>
2022-06-21 20:02:30 +0100

    Algo-3 Solution

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,24 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+    
+    if (haystack != undefined){
+
+        if (!Array.isArray(haystack[0])){ // If hatstack is a single row
+            return haystack.lastIndexOf(needle)
+        }
+
+        let lastHaystack = haystack.length - 1
+
+        for (let hayStackRow = lastHaystack; hayStackRow >= 0; hayStackRow--){
+
+            let needleIndex = haystack[hayStackRow].lastIndexOf(needle)
+
+            if(needleIndex !== -1) { // If needle is found
+                return [hayStackRow, needleIndex]
+            }
+        }
+    }
+    return -1
+}
 
 module.exports = listSorting;
+
