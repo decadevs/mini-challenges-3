7164850cfe326ad16eaeacc6b97e06de404723a6
Darth Vve <oshevire.majoroh@decagon.dev>
2022-06-21 15:58:06 +0100

    Version 1

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,15 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+  let binary = parseInt(value).toString(2);
+  let pad = ''
+
+  if (binary.length < 8) {
+    pad = binary.padStart(8, "0");
+  }
+  let reverse = (pad || binary).split('').reverse().join("");
+  return parseInt(reverse, 2).toString();
+}
 
 module.exports = binaryReversal;
--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,17 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+  let lastIdx, idx;
+  if (typeof haystack[0] != "object") {
+    return haystack.lastIndexOf(needle);
+  } else {
+      for (let i = haystack.length -1; i >= 0; i--) {
+        idx = haystack[i];
+        lastIdx = idx.lastIndexOf(needle);
+        if (haystack.length !== -1) {
+          return [i, lastIdx];
+        }
+      }
+      return -1;
+  }
+}
 
 module.exports = listSorting;
