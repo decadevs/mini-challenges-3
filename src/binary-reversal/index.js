d28acc5f468ef453c0a79e60f614138ab4d96a5a
parishub <greatokhawere51@gmail.com>
2022-06-21 16:34:38 +0100

    mini-challenge-3

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,15 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+    let val = parseInt(value)
+  let binNum = val.toString(2)
+
+  let padNum = binNum.padStart(8, 0)
+  let revNum = padNum.split('').reverse().join('')
+
+  let result = parseInt(revNum, 2)
+  return `${result}`
+}
 
 module.exports = binaryReversal;
--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,16 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+  if (!Array.isArray(haystack[0])) {
+    return haystack.lastIndexOf(needle)
+  }
+
+  for (let a = haystack.length - 1; a >= 0; a--) {
+    let b = haystack[a].lastIndexOf(needle)
+    if (b !== -1) {
+      return [a, b]
+    }
+  }
+
+  return -1
+}
 
 module.exports = listSorting;
