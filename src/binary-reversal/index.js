30905d374011832e13c85d7b7851e18e0c27e7ba
nurez305 <saliu.nurudeen@yahoo.com>
2022-06-21 21:55:29 +0100

    solution to mini challenge 3

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,16 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+    let convert = Number(value)
+    let binaryNum = convert.toString(2)
+    let totalLength = binaryNum.padStart(8, 0);
+    let reverseNum = totalLength.split('').reverse().join('')
+    let convertToDecimal = parseInt(reverseNum, 2)
+
+return `${convertToDecimal}`
+
+}
+console.log(binaryReversal('100'))
 
 module.exports = binaryReversal;
