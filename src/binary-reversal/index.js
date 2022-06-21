5fbd51467acf617fbfeceae13e7c78112471b769
mac <mac@macs-MBP.lan>
2022-06-21 00:59:49 -0700

    completed challenge 3

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,12 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+  const PAD = 8;
+  return parseInt(
+    parseInt(value).toString(2).padStart(PAD, "0").split("").reverse().join(""),
+    2
+  ).toString();
+}
 
 module.exports = binaryReversal;
--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,15 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+  let row = haystack.length - 1;
+  // for flat haystack array
+  if (typeof haystack[0] !== "object") {
+    return haystack.lastIndexOf(needle);
+  }
+  //multidimension array
+  for (let i = row; i >= 0; i--) {
+    if (haystack[i].lastIndexOf(needle) !== -1)
+      return new Array(i, haystack[i].lastIndexOf(needle));
+  }
+  return -1;
+}
 
 module.exports = listSorting;
