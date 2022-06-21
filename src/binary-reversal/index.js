a210a5c12f240a0fd838b5f8a8f734404e875ef1
beastlywallace <akonakpevwe@gmail.com>
2022-06-21 23:35:36 +0100

    mini-challenges 3

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,11 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+ function binaryReversal(value) {
+
+  let numberValue = Number(value).toString(2).padStart(8, 0).split('').reverse().join("");
+  let result = parseInt(numberValue, 2).toString();
+  return result;
+ }
 
 module.exports = binaryReversal;
