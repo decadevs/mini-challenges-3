1265c10481a43241e85e1b35a59a34320196ea0c
stephen Ahaiwe <stephen.ahaiwe@decagon.dev>
2022-06-22 12:32:35 +0100

    updated mini challenge-3

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,18 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+
+  let toBinary = Number(value).toString(2)
+  let eightDigits = `0000000${toBinary}`.slice(-8)
+  let toReversed ="";
+  for(let i = eightDigits.length-1;i>=0;i--){
+     toReversed +=eightDigits[i];
+  }
+ let toDecimal = parseInt(toReversed,2);
+
+ return String(toDecimal);
+
+}
 
 module.exports = binaryReversal;
--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,24 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+  let row = -1, col = -1, computed = [row,col], length = haystack.length;
+    for(let i=0;i<length;i++){
+        if(Array.isArray(haystack[i])){
+            let secondlength = haystack[i].length;
+            for(let j=0;j<secondlength;j++){
+                if(needle === haystack[i][j]){
+                    row = i;
+                    col = j;
+                    computed = [i,j];
+                }
+            }
+        }else if(needle === haystack[i]){
+            row =i;
+        }
+    }
+    if(col === -1){
+        return row;
+    }else{
+        return computed;
+    }
+}
 
 module.exports = listSorting;
