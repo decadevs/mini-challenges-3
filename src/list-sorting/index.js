6eb3450ecc91caa3373a90acde6dd976017a1d2f
profCodey <otunaiyademilade@gmail.com>
2022-06-22 12:30:19 +0100

    Demilade Otunaiya Mini-challenges-3 Solution

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,16 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+  if(!Array.isArray(haystack[0])){
+    return haystack.lastIndexOf(needle)
+}
+else{
+    for (let row = haystack.length-1; row >= 0; row--){
+        let column = haystack[row].lastIndexOf(needle)
+            if (column !==-1){
+                return [row, column]
+            }
+    }
+    return -1
+}
+}
 
 module.exports = listSorting;
