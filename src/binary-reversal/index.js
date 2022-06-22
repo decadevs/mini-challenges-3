a4581ca049c136eb2227cbed2dfe6e2dcb70e787
tobbyano1234 <adegbesan86@gmail.com>
2022-06-22 16:48:45 +0100

    challenge 3 solution

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,14 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+  let num = +value;
+  let binaryNum = num.toString(2).padStart(8, 0);
 
+  binaryNum = binaryNum.split("").reverse().join("");
+
+  binaryNum = parseInt(binaryNum, 2).toString();
+
+  return `${binaryNum}`;
+}
 module.exports = binaryReversal;
--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,19 @@
-function listSorting(needle, haystack) {}
+// const arr = [[0], [1]];
+
+function listSorting(needle, haystack) {
+  if (!Array.isArray(haystack[0])) {
+    return haystack.lastIndexOf(needle);
+  }
+
+  for (let r = haystack.length - 1; r >= 0; r--) {
+    let c = haystack[r].lastIndexOf(needle)
+    if (c !== -1) {
+      return [r, c]
+    }
+  }
+  return -1;
+}
+
+// console.log(listSorting(0, arr))
 
 module.exports = listSorting;
