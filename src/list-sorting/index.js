701df5947100a836924b91e461233f92baa493c2
Dec <dec@GentleStorms-Air.local>
2022-06-22 16:51:53 +0100

    challenge 3 question two update

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,22 +1,43 @@
 function listSorting(needle, haystack) {
-    let index;
+    let index
+    
     for (let i = 0; i < haystack.length; i++) {
-        if (typeof haystack === 'object') {
+        
+        
+        if (haystack[i][i] === undefined) {
+             index = (haystack.lastIndexOf(needle))
+        
+            return index
+            
+        } else {
+            
             //multidimentional array
             haystack.forEach((r, r_index) => {
+                // console.log(r.lastIndexOf(r_index))
                 r.forEach((c, c_index) => {
-                    index = [r_index, c_index]
+                    if (c === needle) {
+                        index = [r_index, c_index]
+                        return index
+                    } 
+                
                 })
-                if (r === needle) {
-                    index = r_index;
-                }
             });
-            {
-        
-            }
+            return index
         }
-    } return index
+    }
+    if (Array.isArray(haystack) && !haystack.length) {
+            index = -1
+            return index;
+        }
 }
 
-
+console.log(listSorting(1,
+    [
+        [2],
+        [2000, 200, 20000],
+        [200, 20],
+        [200, 200, 200, 200, 200, 200, 200, 200, 200, 200],
+        [200, 200, 200, 200, 200],
+        [200, 200, 200, 200, 200, 200],
+      ]))
 module.exports = listSorting;
