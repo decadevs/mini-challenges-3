75bda6f82fe5ae7c02e32c1383da168d643afeee
anthonyponplex <mayakianthony@yahoo.com>
2022-06-22 13:49:43 +0100

    Mini Challenge 2

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,16 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+  if (!Array.isArray(haystack[0])) {
+    return haystack.lastIndexOf(needle)
+  }
+
+  for (let row = haystack.length - 1; row >= 0; row--) {
+    let col = haystack[row].lastIndexOf(needle)
+    if (col !== -1) {
+      return [row, col]
+    }
+  }
+
+  return -1
+}
 
 module.exports = listSorting;
