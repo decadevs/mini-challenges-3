8acc386586592236f0eb5a2b6c7cd2c5ad262a49
Darth Vve <oshevire.majoroh@decagon.dev>
2022-06-22 15:51:38 +0100

    Version 1.1

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -4,14 +4,15 @@
  *  * @param {string} value
  */
 function binaryReversal(value) {
-  let binary = parseInt(value).toString(2);
+  let binary = parseInt(value).toString(2);  //Converts value to binary
   let pad = ''
 
   if (binary.length < 8) {
-    pad = binary.padStart(8, "0");
+    pad = binary.padStart(8, "0");  //Pads it with zeros
   }
+  //Splits the  value into an array reverses it converts it to a string
   let reverse = (pad || binary).split('').reverse().join("");
-  return parseInt(reverse, 2).toString();
+  return parseInt(reverse, 2).toString(); //Converts the number to a decimal
 }
 
 module.exports = binaryReversal;
--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,16 +1,16 @@
 function listSorting(needle, haystack) {
-  let lastIdx, idx;
-  if (typeof haystack[0] != "object") {
-    return haystack.lastIndexOf(needle);
-  } else {
-      for (let i = haystack.length -1; i >= 0; i--) {
-        idx = haystack[i];
-        lastIdx = idx.lastIndexOf(needle);
-        if (haystack.length !== -1) {
-          return [i, lastIdx];
-        }
+  let lastIdx, idx, res = -1;
+  if (Array.isArray(haystack[0])) { //Checks if haystack is an array of an array
+    for (let i = 0; i < haystack.length; i++) {
+      idx = haystack[i];
+      lastIdx = idx.lastIndexOf(needle)
+      if (lastIdx > -1) { //Fetches the result
+        res = [i, lastIdx];
       }
-      return -1;
+    }
+    return res;
+  } else {
+      return haystack.lastIndexOf(needle);
   }
 }
 
