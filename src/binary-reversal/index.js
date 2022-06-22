c4ca646c84c09b60ee8ff115ce69154b5918b740
Folashy <100294216+Folashy@users.noreply.github.com>
2022-06-22 12:16:14 +0100

    Folashade Ayanda git assignment Task 3

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,12 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
-
+function binaryReversal(value) {
+   
+        let input = parseInt(value);
+        let binaryInput = input.toString(2);
+        let reversed = binaryInput.padStart(8,0).split('').reverse().join('');
+        let output = parseInt(reversed, 2);
+        return output.toString();    
+}
 module.exports = binaryReversal;
