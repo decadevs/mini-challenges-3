bd28eff34f90b2057907c778688a2b9d6aac331e
Anuoluwapo <oderinloanuoluwapo@gmail.com>
2022-06-22 10:43:00 +0100

    Anuoluwapo Just completed Challenge 3

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,14 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+    let result = (+value).toString(2).padStart(8, 0).split('').reverse().join('').toString();
+    result = parseInt(result, 2) + '';
+    return result
+
+
+}
 
 module.exports = binaryReversal;
+
+// binaryReversal("10")
--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,44 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+
+    let arr = [];
+    const arr2 = []
+    let emptyArr = [];
+    let column = [];
+    let row = []
+    let result = -1;
+
+
+
+    haystack.forEach((el, i) => {
+
+        if (Array.isArray(el)) {
+            // console.log('yes its an array');
+            if (el.length == 0) {
+                result = -1;
+            } else {
+                arr.push(...el);
+                el.forEach((el2, i2) => {
+                    if (needle == el2) {
+                        column.push(i2);
+                        row.push(i)
+                    }
+                })
+            }
+        } else {
+            result = haystack.lastIndexOf(needle)
+        }
+
+    });
+
+    if (column.length != 0) {
+        result = [row[row.length - 1], column[column.length - 1]]
+        return result
+    } else {
+        return result
+    }
+
+}
 
 module.exports = listSorting;
+
+
