7ae609287144a96fd9331984f13815349ec43144
Dec <dec@Excel.local>
2022-06-22 14:38:34 +0100

    Challenge Three Solution

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,12 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+  let num = parseInt(value);
+  let BinaReversal = num.toString(2).padStart(8, 0).split('').reverse('').join('');
+  let finalReversal = parseInt(BinaReversal, 2)
+  let output = `${finalReversal}`
+  return output
+}
 
 module.exports = binaryReversal;
