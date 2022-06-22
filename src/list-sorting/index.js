76c00f794514772c2b609f70871644da3b0c35e6
Precious2912 <preciousovat@gmail.com>
2022-06-22 14:40:43 +0100

    algorithm solution to mini-challenges-3

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,22 +1,20 @@
 function listSorting(needle, haystack){
+  let multiDimArrIndex = []
   //handles a single array
-  if (!typeof haystack[0] == "object"){
-      return haystack.lastIndexOf(needle)
+  if (!Array.isArray(haystack[0])){
+    return haystack.lastIndexOf(needle)
   }
   //handles multi-dimensional array
 
   for (let row = haystack.length - 1; row >= 0; row--){
     let column = haystack[row].lastIndexOf(needle);
-    //check if number exists
       if (column !== -1){
-        return [row, column];
+        multiDimArrIndex.push(row);
+        multiDimArrIndex.push(column);
+        return multiDimArrIndex;
       }
   }
-  //if number does not exist
   return -1;
 }
-
-//Error - haystack[row].lastIndexOf is not a function
-// haystack[row] is an array, tried it and it worked but it's throwing an error here. 
 
 module.exports = listSorting;
