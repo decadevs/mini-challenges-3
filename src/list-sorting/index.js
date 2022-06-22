f3e82e1a654fb5528a6650cb31c6df06aea3a9c1
jamesobed <jamesobed4@gmail.com>
2022-06-22 13:56:50 +0100

    Challenge 3 list sorting solution first attempt

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,14 @@
-function listSorting(needle, haystack) {}
-
+function listSorting(needle, haystack) {
+    if (!Array.isArray(haystack[0])) {
+        return haystack.lastIndexOf(needle)
+      }
+      for (let row = haystack.length - 1; row >= 0; row--) {
+          let col = haystack[row].lastIndexOf(needle)
+          if (col !== -1) {
+            return [row, col]
+          }
+        }
+        return -1;
+}
+console.log( listSorting(2, [1,3,4,5,2,3,4,5,6,2]))
 module.exports = listSorting;
