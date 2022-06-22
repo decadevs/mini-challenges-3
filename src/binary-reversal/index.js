6b15431bf43c812f80622d7f2d351cc511079bb9
Franklin Osigwe <franklindutch1@gmail.com>
2022-06-22 11:36:49 +0100

    task-3

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,19 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+    
+    let number = parseInt(value) // changed a string with a num
+        let binNum = number.toString(2)// to a binary number 
+    
+        let paddedNumber = binNum.padStart(8,'0') //padded to the nearest N \* 8 bits
+        let reverseNum = paddedNumber.split('').reverse().join('') // split the binary numbers,reverse and join the strings s
+    
+        return parseInt(reverseNum, 2).toString();
+    
+  
+}
+console.log(binaryReversal("200"))
 
 module.exports = binaryReversal;
+
