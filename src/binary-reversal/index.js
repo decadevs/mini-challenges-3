831498bdc87758acea298ef80db297db7e5b6f88
christopher-moses <christmoses1999@icloud.com>
2022-06-22 13:18:46 +0100

    my-assignment

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,19 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value){
+ let number = parseInt(value);
+ let binary = number.toString(2);
+
+ let paddedNumber = binary.padStart(8,0)
+ let reverseNumber = paddedNumber.split('').reverse().join('');
+ let result = parseInt(reverseNumber,2);
+ return result
+// let binaryValue = Number(value).toString(2);
+//   let paddedValue = binaryValue.padStart(8, '0');
+//   let reversedStr = paddedValue.split('').reverse().join('');
+//   return (parseInt(reversedStr, 2)).toString();
+
+}
 
 module.exports = binaryReversal;
