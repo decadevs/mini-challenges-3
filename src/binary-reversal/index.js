d6c8f8dcf47d6575cb6306a8ca3c0b168fe78908
Precious2912 <preciousovat@gmail.com>
2022-06-22 14:01:07 +0100

    algorithm solution to mini-challenges-3

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,27 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+  //convert value to binary
+  value = Number(value)
+  value = value.toString(2)
+  //convert value to string, store in an array
+  let binary = String(value).split("");
+  console.log(binary)
+  //loop through array to check if it is 8 digits
+  for (let i = 0; i < binary.length; i++){
+    //if not, add Zeros before current number
+    if (binary.length < 8){
+      binary.unshift("0");
+    }
+  }
+  //reverse digits in array
+  binary.reverse();
+  //convert arr to string
+  binary = binary.join("")
+  //convert binary to decimal
+  let decimal = parseInt(binary, 2)
+  return String(decimal);
+}
 
 module.exports = binaryReversal;
--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,22 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack){
+  //handles a single array
+  if (!typeof haystack[0] == "object"){
+      return haystack.lastIndexOf(needle)
+  }
+  //handles multi-dimensional array
+
+  for (let row = haystack.length - 1; row >= 0; row--){
+    let column = haystack[row].lastIndexOf(needle);
+    //check if number exists
+      if (column !== -1){
+        return [row, column];
+      }
+  }
+  //if number does not exist
+  return -1;
+}
+
+//Error - haystack[row].lastIndexOf is not a function
+// haystack[row] is an array, tried it and it worked but it's throwing an error here. 
 
 module.exports = listSorting;
