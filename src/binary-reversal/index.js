85b12bc23c50576516e1f2d545b50bae2f33b743
jamesobed <jamesobed4@gmail.com>
2022-06-22 01:41:09 +0100

    3rd mini challenge solution 1

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,16 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+    let number = parseInt(value)
+    let binaryNumber = number.toString(2)
+
+    let paddedNumber = binaryNumber.padStart(8,0)
+    let reversedNumber = paddedNumber.split('').reverse().join('')
+    let myOutput = parseInt(reversedNumber, 2)
+    myOutput.toString()
+
+    return `${myOutput}`
+}
 
 module.exports = binaryReversal;
