449aafa4baecf153bfc3ea830053e54a6a9e153c
Dec <dec@GentleStorms-Air.local>
2022-06-22 15:59:57 +0100

    challenge 3

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,22 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+    let index;
+    for (let i = 0; i < haystack.length; i++) {
+        if (typeof haystack === 'object') {
+            //multidimentional array
+            haystack.forEach((r, r_index) => {
+                r.forEach((c, c_index) => {
+                    index = [r_index, c_index]
+                })
+                if (r === needle) {
+                    index = r_index;
+                }
+            });
+            {
+        
+            }
+        }
+    } return index
+}
+
 
 module.exports = listSorting;
