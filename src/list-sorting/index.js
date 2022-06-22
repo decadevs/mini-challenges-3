466006f7f7719664507c83ab8f1a3ca792e8ade3
Dec <dec@Richie.local>
2022-06-22 14:24:24 +0100

    Solution to Challenge 3

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,19 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+  if(!Array.isArray(haystack[0])){
+    return haystack.lastIndexOf(needle);
+}
+
+ for(let row= haystack.length-1; row>=0; row--){
+
+  let col= haystack[row].lastIndexOf(needle)
+  if(col!=-1){
+    return [row, col]
+  }
+
+
+ }
+return -1
+
+}
 
 module.exports = listSorting;
