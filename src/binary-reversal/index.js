2aebb9a824c8a61e62f82d4337d285793b93130f
babsddev <ajayi.gbemi0806@gmail.com>
2022-06-22 14:04:20 +0100

    Solution to week 1 algorithim mini-challenge-3

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,14 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+  let num = parseInt(value);
+  let binary = num.toString(2);
+  let numPadded = binary.padStart(8, 0);
+  let reversal = numPadded.split("").reverse().join("");
+  let answer = parseInt(reversal, 2);
+
+  return `${answer}`;
+}
 
 module.exports = binaryReversal;
