4608b65aea4854b958e012c71cdb0ce15a11094c
Vincent Agbapulonwu <vincentmicheal56@gmail.com>
2022-06-22 09:30:14 +0100

    Solutions for Mini-Challenges 3

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,15 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+
+    let binaryValue = Number(value).toString(2);
+    let paddingLength = binaryValue.length % 8 === 0 ? 0 : 8 - binaryValue.length % 8
+    let paddedReversedBinaryValue = [...Array(paddingLength).fill(0), ...binaryValue].reverse().join('');
+
+  let parseIntValue = parseInt(paddedReversedBinaryValue,2);
+  return parseIntValue.toString();
+
+}
 
 module.exports = binaryReversal;
