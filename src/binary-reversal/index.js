211c564d823be1cb451b9af4fa1900cd8911d334
Dec <dec@macs-MacBook-Air-10.local>
2022-06-22 14:44:40 +0100

    challenge 3

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,19 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+  let input = parseInt(value, 10).toString(2);
+  const inputLength = input.length;
+
+  const byteLen = 8 - (inputLength % 8);
+  const num = 0;
+  inputLength % 8 !== 0
+    ? (input = new Array(byteLen + 1).join(num) + input)
+    : input;
+  input = input.split("").reverse().join("");
+  input = parseInt(input, 2).toString();
+  return input;
+}
 
 module.exports = binaryReversal;
+console.log(binaryReversal("213"));
