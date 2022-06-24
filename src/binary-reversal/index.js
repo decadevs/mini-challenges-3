1b2172335261e509e1533451e646ecd2c81bcb12
tofarati <oluwatofarati.agbaje@decagon.dev>
2022-06-24 23:25:24 +0100

    Removed unnecessary code for padStart

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -7,13 +7,7 @@ function binaryReversal(value)
 {
   const num = parseInt(value);
   let binaryStr = num.toString(2);
-
-  if (binaryStr.length % 8 !== 0)
-  {
-    const fullLength = 8 - (binaryStr.length % 8) + binaryStr.length;
-    binaryStr = binaryStr.padStart(fullLength, '0');
-  }
-
+  binaryStr = binaryStr.padStart(Math.ceil(binaryStr.length / 8) * 8, '0');
   const reversed = binaryStr.split('').reverse().join('');
   return String(parseInt(reversed, 2));
 }
