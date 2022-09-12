b24b3ebfc892a17487bb764b054741ebc01d0f42
skippergoroye <oluwatosinadegoroye21@gmail.com>
2022-09-12 17:03:16 +0100

    challenge 3 solution

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,28 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+// function binaryReversal(value) {}
 
-module.exports = binaryReversal;
+// module.exports = binaryReversal;
+
+
+function binaryReversal(str){
+    let inputBinaryStr = parseInt(str, 10).toString(2);
+    let inputBinaryLength = inputBinaryStr.length;
+
+    // loading zeroes to make the number an integral number of bytes
+
+    let bytelen = 8 - (inputBinaryLength % 8);
+    let x = 0;
+
+    inputBinaryLength % 8 !== 0 ? inputBinaryStr = new Array(bytelen+1) + inputBinaryStr : inputBinaryStr
+    inputBinaryStr = inputBinaryStr
+    .split("")
+    .reverse()
+    .join("")
+
+    inputBinaryStr = parseInt(inputBinaryStr, 2)
+    return inputBinaryStr
+}
+
+console.log(binaryReversal(`11110100`))
