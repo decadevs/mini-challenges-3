0054e0183691c2e83f32b29f92561e315e5e2210
Decagon <decagon@Decagons-MacBook-Air.local>
2022-09-13 20:53:23 +0100

    final commit

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -8,32 +8,13 @@
         let toBinary = parseInt(value).toString(2);
         // padding of the binary to 8 bits.
         let padBinary = toBinary.padStart(8,0);
-        // reverse padded binary
-        let reverseBinary = padBinary.split('').reverse().join('')
-    
-     let result = parseInt(reverseBinary, 2);
-    
-        return `${result}`
+        // split the padded binary,reverse and join.
+        let reversedBinary = padBinary.split('').reverse().join('')
+    // convert the reversed binary into digits and store in a variable finalNum
+     let finalNum = parseInt(reversedBinary, 2);
+    // return the final result
+     return `${finalNum}`
+     
       }
-    
-    
-    //   function binaryReversal(str){
-    //     let inputBinaryStr = parseInt(str, 10).toString(2);
-    //     let inputBinaryLength = inputBinaryStr.length;
-    
-    //     // loading zeroes to make the number an integral number of bytes
-    
-    //     let bytelen = 8 - (inputBinaryLength % 8);
-    //     let x = 0;
-    
-    //     inputBinaryLength % 8 !== 0 ? inputBinaryStr = new Array(bytelen+1) + inputBinaryStr : inputBinaryStr
-    //     inputBinaryStr = inputBinaryStr
-    //     .split("")
-    //     .reverse()
-    //     .join("")
-    
-    //     inputBinaryStr = parseInt(inputBinaryStr, 2)
-    //     return inputBinaryStr
-    //}
-    module.exports = binaryReversal;
+      module.exports = binaryReversal;
     
--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,24 +1,19 @@
 function listSorting(needle, haystack) {
-    if (needle.includes(haystack) === true) {
-        return needle.indexOf(haystack)
-    } else {
-        return -1
-    }
-    }
-// const arr = [[0], [1]];
-
-// function listSorting(needle, haystack) {
-//     if (!Array.isArray(haystack[0])) {
-//       return haystack.lastIndexOf(needle);
-//     }
-
-//     for (let r = haystack.length - 1; r >= 0; r--) {
-//       let c = haystack[r].lastIndexOf(needle)
-//       if (c !== -1) {
-//         return [r, c]
-//       }
-//     }
-//     return -1;
-//   }
-
-module.exports = listSorting;
+    // if haystack is not a single array, return last index of haystack(needle).
+        if(!Array.isArray(haystack[0])){
+          return haystack.lastIndexOf(needle)
+      }
+      // loop through the haystack row using for loop in descending order
+      // loop through the haystack column to identify the last index(needle)
+      // if the last index of the colum exists, return row, column, else return -1.
+          for (let row = haystack.length-1; row >= 0; row--){
+              let col = haystack[row].lastIndexOf(needle)
+                  if (col !==-1){
+                      return [row, col]
+                  }
+          }
+          return -1
+      }
+      
+    module.exports = listSorting;
+    
