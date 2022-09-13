3a87c3a66a4ac201380616debb245989bd25f1b8
Decagon <decagon@Decagons-MacBook-Air.local>
2022-09-13 15:51:36 +0100

    initial commit

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,37 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
-
-module.exports = binaryReversal;
+ function binaryReversal(value) {
+    // conversion of integar to binary
+        let toBinary = parseInt(value).toString(2);
+        // padding of the binary to 8 bits.
+        let padBinary = toBinary.padStart(8,0);
+        // reverse padded binary
+        let reverseBinary = padBinary.split('').reverse().join('')
+    
+     let result = parseInt(reverseBinary, 2);
+    
+        return `${result}`
+      }
+    
+    
+    //   function binaryReversal(str){
+    //     let inputBinaryStr = parseInt(str, 10).toString(2);
+    //     let inputBinaryLength = inputBinaryStr.length;
+    
+    //     // loading zeroes to make the number an integral number of bytes
+    
+    //     let bytelen = 8 - (inputBinaryLength % 8);
+    //     let x = 0;
+    
+    //     inputBinaryLength % 8 !== 0 ? inputBinaryStr = new Array(bytelen+1) + inputBinaryStr : inputBinaryStr
+    //     inputBinaryStr = inputBinaryStr
+    //     .split("")
+    //     .reverse()
+    //     .join("")
+    
+    //     inputBinaryStr = parseInt(inputBinaryStr, 2)
+    //     return inputBinaryStr
+    //}
+    module.exports = binaryReversal;
+    
--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,24 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+    if (needle.includes(haystack) === true) {
+        return needle.indexOf(haystack)
+    } else {
+        return -1
+    }
+    }
+// const arr = [[0], [1]];
+
+// function listSorting(needle, haystack) {
+//     if (!Array.isArray(haystack[0])) {
+//       return haystack.lastIndexOf(needle);
+//     }
+
+//     for (let r = haystack.length - 1; r >= 0; r--) {
+//       let c = haystack[r].lastIndexOf(needle)
+//       if (c !== -1) {
+//         return [r, c]
+//       }
+//     }
+//     return -1;
+//   }
 
 module.exports = listSorting;
