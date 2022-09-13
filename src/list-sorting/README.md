e02db79bb958d967069c1d746853e27607d31826
Decagon <decagon@Decagons-MacBook-Air.local>
2022-09-13 17:02:19 +0100

    add feature mini-challenge3(binary-reversal and list-sorting

--- src/list-sorting/README.md
+++ src/list-sorting/README.md
@@ -1,4 +1,4 @@
-## List Sorting
+ ## List Sorting
 
 You have been given an array of size `N` consisting of integers. In addition you have been given an element `M`. You need to find and print the index of the last occurrence of this element `M` in the array if it exists in it, otherwise print `-1`.
 
@@ -28,7 +28,7 @@ needle: 5;
 haystack: [
   [1, 2, 3, 4, 5],
   [1, 2, 3, 4, 5],
-  [1, 2, 3, 4, 5],
+  [1, 2, 3, 4, 5], 
 ];
 ```
 
--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,38 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack){
+if (typeof (haystack[0]) == "object") {
+ let lastArray = -1;
+  let lastIndex;
+  
+  for (let i = 0; i < haystack.length; i++) {
 
-module.exports = listSorting;
+    if (haystack[i].includes(needle)) {
+        lastArray = i;
+        lastIndex = haystack[i].lastIndexOf(needle)
+
+    }
+  }
+  if (lastArray != -1) {
+    return [lastArray, lastIndex]
+  } else {
+    return -1
+  }
+
+}
+else {
+  return haystack.lastIndexOf(needle)
+}
+}
+
+  // console.log(listSorting(5, [1, 2, 3, 4, 5]));
+
+  // console.log(listSorting(5, [1, 2, 3, 4, 4, 4]))
+  // console.log(listSorting(5, [5, 1, 2, 3, 4, 5]))
+  // console.log(listSorting(0, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]))
+  // expect(listSorting(-1, [-1, 0, 10, 10, 10, 1, 1, 1, 1, 1, 1])).toBe(0);
+  // expect(listSorting(2, [2])).toBe(0);
+  // expect(listSorting(1, [])).toBe(-1);
+  // expect(listSorting(3, [-1, 0, 1, 2, 3])).toBe(4);
+  // expect(
+
+
+  module.exports = listSorting;
