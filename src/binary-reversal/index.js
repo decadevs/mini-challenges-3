0321d25b26da11badaaec8bdce5d8bffbca4bc20
22Theresecodes <105355739+22Theresecodes@users.noreply.github.com>
2022-09-13 23:17:26 +0100

    solution to assignment

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,12 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+    let num = parseInt(value)
+    let binaryNum = numToString(2);
+    
+    let padded = binaryNum.padStart(8,0)
+    let reversed = padded.split('').reverse().join
+}
 
 module.exports = binaryReversal;
--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,27 @@
-function listSorting(needle, haystack) {}
-
-module.exports = listSorting;
+function listSorting(needle, haystack) {
+    let row = null;
+    let column = null;
+    let result = null;
+    if (Array.isArray(haystack[0])) {
+      for (let i = 0; i < haystack.length; i++) {
+        for (let j = 0; j < haystack[i].length; j++) {
+          if (haystack[i][j] === needle) {
+            row = i;
+            column = j;
+          }
+        }
+      }
+      
+    } else if (!Array.isArray(haystack[0])) {
+      column = haystack.lastIndexOf(needle);
+    }
+    if (row || row === 0) {
+      result = [row, column];
+    } else result = column;
+    // return result === null ? -1:result
+    if(result === null){
+      return -1;
+    }
+    return result;
+  }
+  module.exports = listSorting;
