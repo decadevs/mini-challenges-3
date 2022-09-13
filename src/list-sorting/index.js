d6579fba458865387eb30fc0d17e417f1f5b3fe2
Oluwaseyi <oluwaseyi.makinde@decagon.dev>
2022-09-13 08:16:26 -0700

    assignment solution mini-challenge3

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,22 @@
-function listSorting(needle, haystack) {}
-
+function listSorting(needle, haystack) {
+    if(typeof(haystack[0]) == "object"){
+        let lastArray = -1;
+        let lastIndex;
+        for(let i = 0; i < haystack.length; i++){
+            if(haystack[i].includes(needle)){
+                lastArray = i;
+                lastIndex = haystack[i].lastIndexOf(needle)
+            }
+        }
+        if(lastArray != -1){
+            return [lastArray, lastIndex]
+        }
+          else{
+            return -1
+         }
+    }
+    else{
+        return haystack.lastIndexOf(needle)
+}
+}
 module.exports = listSorting;
