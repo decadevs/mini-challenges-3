07306553c3636702e92dfbea2f883f474793a664
Mac User <daniel.iwegbue@decagon.dev>
2022-09-13 07:25:56 -0500

    third challenge

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,38 @@
-function listSorting(needle, haystack) {}
+
+
+
+
+
+function listSorting(needle, haystack) {
+let row;
+let col;
+
+if(typeof(haystack[0]) === 'object'){
+
+    for (let i = 0; i < haystack.length; i++) {
+        if(haystack[i].includes(needle)) {
+
+            row = i
+            col = haystack[i].lastIndexOf(needle)
+
+        }
+        
+    }
+    
+    return row !== undefined? [row, col] : -1
+
+}else
+
+
+    return haystack.lastIndexOf(needle)
+
+}
+
+
+
+
+
+
+
 
 module.exports = listSorting;
