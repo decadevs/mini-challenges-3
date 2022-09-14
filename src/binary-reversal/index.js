f16cfb598c15df405e339f98c7f1ef714a2408ab
IjeomaAnyanwu <anyanwu.ijeomajovita@gmail.com>
2022-09-14 12:34:59 +0100

    final-soln

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,15 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+    let binaryNum = parseInt(value)
+        let Binary = binaryNum.toString(2);
+        let padded = Binary.padStart(8, '0');
+        let paddedReversal = padded.split('').reverse().join('');
+        let reverseBinary = parseInt(paddedReversal,2).toString()
+    
+        return reverseBinary;
+    
+}
 
 module.exports = binaryReversal;
