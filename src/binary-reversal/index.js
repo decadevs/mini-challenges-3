b2e33c405577eca2398343e98988f5b7b1b91b64
mac <mac@macs-MacBook-Pro.local>
2022-09-14 12:33:06 +0100

    done

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,28 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+ function binaryReversal(value) {
+    if(!value) return "0";
+    // let value = '225';
+    let num = parseInt(value);
+    let binaryNumber = num.toString(2);
+
+    
+    let paddedBinary;
+    if(binaryNumber.length > 8) {
+        paddedBinary = "0".repeat(8 - (binaryNumber.length % 8)) + binaryNumber;
+    }else {
+        paddedBinary = "0".repeat(8 - binaryNumber.length) + binaryNumber;
+    }
+    console.log(paddedBinary);
+    let reversedBinary = paddedBinary.split("").reverse().join("");
+
+    
+    return parseInt(reversedBinary,2).toString();
+    
+}
+// console.log(binaryReversal("1"));
+
+
 
 module.exports = binaryReversal;
