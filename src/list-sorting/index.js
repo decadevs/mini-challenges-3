353f002bb234d859b3ee8b5c639f6f3396db7b06
KennethJT1 <112853926+KennethJT1@users.noreply.github.com>
2022-09-14 07:03:58 +0100

    Update index.js

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,20 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+  let rowIndex = haystack.length - 1;
+  let colIndex = -1;
+
+  for (let i = haystack.length - 1; i >= 0; i--) {
+    if (Array.isArray(haystack[i])) {
+      rowIndex = i;
+      colIndex = haystack[i].lastIndexOf(needle);
+      if (colIndex != -1) {
+        return [rowIndex, colIndex];
+      }
+    } else {
+      if (haystack[i] == needle) return i;
+    }
+  }
+  return -1;
+}
+console.log(listSorting)
 
 module.exports = listSorting;
