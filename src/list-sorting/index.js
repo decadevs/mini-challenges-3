1e333937a13f5ddfcedd849881dc17130a83ee12
Hopeuche360 <hopechijuka@gmail.com>
2022-09-14 12:33:25 +0100

    submit

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,21 @@
-function listSorting(needle, haystack) {}
+/*My thought process:
+- remove the string by converting the value to integar;
+- convert to binary usingthe toString method;
+- pad the binary num to 8 bits;
+- reverse the binary num;
+- convert the binary num to a decimal num, as well convert the num to string;
+- then return the decimal num;
+*/
+
+function binaryReversal(value) {
+    let toIntegar = parseInt(value);
+    let toBinary = toIntegar.toString(2);
+
+    let paddedNum = toBinary.padStart(8, 0);
+    let reversedNum = paddedNum.split("").reverse().join("");
+
+    let decimalNum = parseInt(reversedNum, 2).toString();
+    return decimalNum;
+};
 
 module.exports = listSorting;
