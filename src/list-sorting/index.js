90275a9fe4fb7f63128540b79a595cf4b5185172
samuelajalode <113217825+samuelajalode@users.noreply.github.com>
2022-11-11 10:25:47 +0100

    Update index.js

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,31 +1,17 @@
-/**
+    /**
  * @param {string} haystack
  * @param {string} needle
  * @return {number}
  */
  function listSorting(haystack, needle) {
-    if (!needle.length) return 0;
+    // code goes here  
+    let arr = haystack.split(",");
+    let sorted = arr.sort();
+    let index = sorted.indexOf(needle);
+    return index;
+  }
 
-    // Loop through the haystack's letters
-    for (let i = 0; i <= haystack.length - needle.length; i++) {
-        // Check if the current letter matches the start of the needle
-        if (haystack[i] === needle[0]) {
-            // Loop through the needle
-            for (let j = 0; ; j++) {
-                // Reached the end of the needle (and thus fully found it at i)
-                if (j == needle.length) {
-                    return i;
-                }
-                // Letters not matched (needle not found at i)
-                else if (haystack[i + j] !== needle[j]) {
-                    break;
-                }
-            }
-        }
-    }
-    return haystack;
-}
+    console.log(listSorting("hello,world,here,i,am", "world"));
 
-console.log(listSorting(5, [1, 2, 3, 4, 5]));
 
 module.exports = listSorting;
