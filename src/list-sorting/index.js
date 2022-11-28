6ba9effdaf5d9c8f49154610b0d130f06cafdcd0
Chidike Henry <30509335+lacegiovanni17@users.noreply.github.com>
2022-11-29 00:36:37 +0100

    Solutions to mini challenges 3
    These are the solutions to the questions in mini challenges 3

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,43 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+  if (haystack.length === 0) {
+    return -1
+  }
+
+  let event = 0;
+  for (let i = 0; i < haystack.length; i++) {
+    if (haystack[i] === needle) {
+      event++;
+    }
+  }
+
+  if (event === 0) {
+    return -1;
+  } else if (event === 1) {
+    return haystack.indexOf(needle)
+  } else {
+    return haystack.lastIndexOf(needle)
+  }
+
+  if (typeof (haystack[0]) === "object") {
+    let column
+    let row
+
+    for (let i = 0; i < haystack.length; i++) {
+      for (let j = 0; j < haystack[i].length; j++) {
+        if (haystack[i][j] === needle) {
+          column = haystack[i].lastIndexOf(needle)
+          row = i
+        }
+      }
+    }
+
+    if (row == undefined) return -1;
+    return [row, column]
+  } else {
+    return haystack.lastIndexOf(needle)
+
+  }
+}
 
 module.exports = listSorting;
+
