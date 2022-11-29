5934af9febe4aefbf847b698a84f9be9303096bd
Bunmi Oladipupo <bunmidavids194@gmail.com>
2022-11-29 21:43:15 +0100

    Challenge-3

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,23 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+ function binaryReversal(value) {
+    var value = Number(value);
+    var valueInBinary = value.toString(2);
+
+    var binArray = valueInBinary.split('');
+    var num = valueInBinary.length
+    console.log(binArray)
+    if (binArray.length < 8) {
+        for (let i = 0; i < (8 - num); i++) {
+            binArray.unshift('0');
+            
+        }
+    }
+    console.log(binArray)
+    var reversal = binArray.reverse();
+    
+    return parseInt(reversal.join(''),2);
+}
 
 module.exports = binaryReversal;
