5bf81a7da3a239a566ea9e24dc4dda0b8f0921a3
Akingbade Motolani <akingbademotolani@yahoo.co.uk>
2022-11-30 22:13:32 +0100

    finishing challenge 3

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,26 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
-
+function binaryReversal(value) {
+    //account for if a user inputs a negative integer
+    let num = Number(value)
+    if(num < 0){
+        return "invalid"
+    }
+    //convert number to binary
+let bi = num.toString(2);
+console.log(bi)
+// let padBinary = '00'+ binary
+let padBi = bi.padStart(8, 0);
+//let padBinary = padStart(3,"00")
+console.log(padBi);
+//reverse the converted number
+let reversebinary = padBi.split('').reverse();
+//join and then turn to DECIMAL number
+let binaryStri = reversebinary.join('')
+let finalNum = parseInt(binaryStri,2)
+let final = `${finalNum}`
+return final
+}
+console.log(binaryReversal("4567"))
 module.exports = binaryReversal;
